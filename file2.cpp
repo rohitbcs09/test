6f430dc830baf42516de70bb9d09851e57553a83
Rohit Kumar <rokkumar@cs.stonybrook.edu>
2018-03-13 17:35:58 -0400

    added another line

--- file2.cpp
+++ file2.cpp
@@ -2,5 +2,6 @@
 #include <stdio.h>
 
 int main() {
-    std::cout << "Hello World in file 2!!!"<< "\n";
+    std::cout << "Line 1: Hello World in file 2!!!"<< "\n";
+    std::cout << "Line 2: Hello World in file 2!!!"<< "\n";
 }
